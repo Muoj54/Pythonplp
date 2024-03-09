90be5504a04e32160c1f04464baaf9550b37e45d
Muoj54 <160734738+Muoj54@users.noreply.github.com>
2024-03-09 16:51:11 +0300

    This is my first python commit

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+location = input("Where do you live?: ")
+print("Hello {}, you are {} years old and live in {}.".format(name, age, location))
